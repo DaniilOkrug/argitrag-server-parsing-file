6a074bd3f3eca784b6802cdee7ee802af70321fe
perassi <horacio2jp@gmail.com>
2022-08-08 17:57:36 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const fs = require("fs");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  const fileData = fs.readFileSync("../result.json", "utf8");
+
+  res.json(fileData);
+});
+
+const start = async () => {
+  try {
+    server.listen(PORT, () =>
+      console.log(`Server started in development mode on PORT = ${PORT}`)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
